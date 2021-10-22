b4ec719d7dfbc6b6f0506576320c5ac31f3d6366
Luke <Lukebone@users.noreply.github.com>
2021-10-22 15:11:03 -0400

    Website update

--- docs/assets/js/bs-init.js
+++ docs/assets/js/bs-init.js
@@ -77,4 +77,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 })();
+
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	  return new bootstrap.Tooltip(tooltipTriggerEl);
+	})
 }, false);
