72b1d7f7b87623b34681dac2a8742cc61c57adfd
Adonay Pichardo <adonaypichardo@gmail.com>
2021-10-25 16:51:55 -0400

    created template script for selecting entire data base: getDatabase.py, need to work on this and give it to Jared A$AP rocky

---
+++ src/python_scripts/getDatabase.py
@@ -0,0 +1,59 @@
+# <Document_Header Start>
+"""
+filename : getDatabase.py
+author : Adonay Pichardo
+description :
+"Returns entire Thor Database as a 2d list"
+"""
+# <Document_Header End>
+
+# <Standard Imports Start>
+# List all imports alphabetically for Python3 standard libraries
+import mysql
+# <Standard Imports End>
+
+
+# <Internal Imports Start>
+# List all imports alphabetically for libraries authored for Thor
+# NONE
+# <Internal Imports End>
+
+# <External Imports Start>
+# List all imports alphabetically for libraries NOT authored for Thor
+# NONE
+# <External Imports End>
+
+# <Global Objects Start>
+# NONE
+# <Global Objects End>
+
+# <Classes Start>
+# NONE
+# <Classes End>
+
+# <Functions Start>
+def getAllData():
+    data = []
+
+    ################################
+    # Open connection to database
+    ################################
+
+    ################################
+    # Get all rows
+    ################################
+
+    ################################
+    # Close connection to database
+    ################################
+
+    ################################
+    # Put all data fields of each row into their own list
+    ################################
+
+    ################################
+    # Insert each row into data
+    ################################
+
+    return data
+# <Functions End>
