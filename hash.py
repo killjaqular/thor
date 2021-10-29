628fdeb693063521642c950a9bc2899c5bf6f975
Adonay Pichardo <adonaypichardo@gmail.com>
2021-10-29 15:18:14 -0400

    removed hash.py

--- hash.py
+++
@@ -1,9 +0,0 @@
-# importing the library that holds md5
-from hashlib import md5
-
-# getting our random number from the user for proof of concept
-randomNumber = input('Enter a random number: ')
-
-# md5 hash of the random number
-hashObj = md5(randomNumber.encode())
-print(hashObj.hexdigest())
